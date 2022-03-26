ac21f3170a4036b4fb26ed19aaceef0aa0ef336b
qinzhiwei1 <qinzhiwei1@tal.com>
2022-03-26 23:37:45 +0800

    jsdoc

---
+++ jsdoc-demo/index.js
@@ -0,0 +1,11 @@
+/**
+ * Represents a book.
+ * @constructor
+ * @param {string} title - The title of the book.
+ * @param {string} author - The author of the book.
+ */
+ function Book(title, author) {
+}
+
+
+// 需要配置jsdoc包和vscode插件使用
